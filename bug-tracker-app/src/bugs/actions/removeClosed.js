db81a1bda46289489dd8beddbc3dbe02bc0a4b69
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-11 16:38:47 +0530

    logger & stateMiddleware

--- bug-tracker-app/src/bugs/actions/removeClosed.js
+++ bug-tracker-app/src/bugs/actions/removeClosed.js
@@ -1,5 +1,8 @@
-export function removeClosed(bugs /* To be fixed */){
-    const bugsToRetain = bugs.filter(bug => !bug.isClosed);
-    const action = { type : 'INIT_BUG', payload : bugsToRetain };
-    return action;
+export function removeClosed(){
+    return function(storeState){
+        const bugs = storeState;
+        const bugsToRetain = bugs.filter(bug => !bug.isClosed);
+        const action = { type : 'INIT_BUG', payload : bugsToRetain };
+        return action;
+    }
 }
--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,7 +1,36 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import bugsReducer from '../reducers/bugsReducer';
 
-const store = createStore(bugsReducer);
+
+/* function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.log(action);
+            next(action);
+        }
+    }
+} */
+
+const loggerMiddleware = store => next => action => {
+    console.group(action.type);
+    console.log('before -> ', store.getState());
+    console.log('action -> ', action);
+    next(action);
+    console.log('after -> ', store.getState());
+    console.groupEnd();
+};
+
+const stateMiddleware = store => next => action => {
+    if (typeof action === 'function'){
+        const storeState = store.getState();
+        const actionObj = action(storeState);
+        next(actionObj);
+    } else {
+        next(action);
+    }
+}
+
+const store = createStore(bugsReducer, applyMiddleware(loggerMiddleware, stateMiddleware));
 
 export default store;
 
