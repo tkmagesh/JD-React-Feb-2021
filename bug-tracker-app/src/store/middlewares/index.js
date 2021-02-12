13d749f377bcf54dc4327e8e50c6d5dd41f470b0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-12 16:14:47 +0530

    using redux-logger & redux-thunk

--- bug-tracker-app/src/store/middlewares/index.js
+++ bug-tracker-app/src/store/middlewares/index.js
@@ -1,9 +1,17 @@
 import { applyMiddleware } from 'redux';
 
+/* 
 import loggerMiddleware from './loggerMiddleware';
 import asyncMiddleware from './asyncMiddleware';
 import promiseMiddleware from './promiseMiddleware';
 
 const rootMiddleware = applyMiddleware(loggerMiddleware, asyncMiddleware, promiseMiddleware);
+*/
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promiseMiddleware';
+
+const rootMiddleware = applyMiddleware(logger, thunk, promiseMiddleware);
 
 export default rootMiddleware;
